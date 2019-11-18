6cc550187f5074ba94a8bfebdcd76d21a182dd60
Yashasvi Ranjan <yashasvi1ranjan@gmail.com>
2019-11-18 12:47:16 +0530

    Update newPolyfill.js

--- src/Inheritance/newPolyfill.js
+++ src/Inheritance/newPolyfill.js
@@ -5,11 +5,11 @@ sampleClass.prototype.getCname = function() {
   return this.cname;
 };
 
-const _new = function(className , ...args){
+const _new = function(className, ...args) {
   let classObj = Object.create(className.prototype);
-  classObj.call(classObj , args);
+  className.call(classObj, args);
   return classObj;
-}
+};
 
 const newPolyfill = function(fname) {
   if (typeof fname !== "function")
@@ -32,17 +32,15 @@ const newPolyfill = function(fname) {
 };
 let newObj = new sampleClass();
 console.log("new obj", newObj);
-console.log(newObj instanceOf sampleClass);//true
-console.log("new cname", newObj.getCname());//sample
+console.log(newObj instanceof sampleClass); //true
+console.log("new cname", newObj.getCname()); //sample
 
 let newPolyObj = newPolyfill(sampleClass);
 console.log("new poly obj", newPolyObj);
-console.log(newPolyObj instanceOf sampleClass);//true
-console.log("newPly cname", newPolyObj.getCname());//sample
+console.log(newPolyObj instanceof sampleClass); //true
+console.log("newPly cname", newPolyObj.getCname()); //sample
 
 let _newObj = _new(sampleClass);
-console.log("_new obj",_newObj);
-console.log(_newObj instanceOf sampleClass);//true
-console.log("_new cname",_newObj.getCname());//sample
-
-
+console.log("_new obj", _newObj);
+console.log(_newObj instanceof sampleClass); //true
+console.log("_new cname", _newObj.getCname()); //sample
