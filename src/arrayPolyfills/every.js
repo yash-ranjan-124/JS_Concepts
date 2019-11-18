812d76950e1f1c446b6738237aa88780a71dd535
Yashasvi Ranjan <yashasvi1ranjan@gmail.com>
2019-11-18 13:32:13 +0530

    Update every.js

--- src/arrayPolyfills/every.js
+++ src/arrayPolyfills/every.js
@@ -1,1 +1,24 @@
+const arr = [6, 7, 2, 4, 10];
 
+const Every = function(callback) {
+  let arr = this;
+  for (let k = 0; k < arr.length; k++) {
+    if (!callback(arr[k], k)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+arr.__proto__.Every = Every;
+
+let response1 = arr.Every(function(current, index) {
+  return current > 1;
+});
+
+let response2 = arr.every(function(current, index) {
+  return current > 1;
+});
+
+console.log(response1);//true
+console.log(response2);//true
